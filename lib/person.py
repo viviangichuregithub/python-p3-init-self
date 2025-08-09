904059a7408c94b8343c161cd15366d0be04d3f7
Vivian Gichure <viviangichure@gmail.com>
2025-08-09 14:35:59 +0300

    completed lab

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
